369f6a0fadb89346ea59b083b2c258b449fa0c1e
Joshua McCollum <xmtuner629@yahoo.com>
2021-08-21 15:09:12 -0500

    Finished assignment with stretch goals

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -100,25 +100,89 @@ console.log('is Liver in favoriteFoods?', find( 'Liver', favoriteFoods));
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
+  console.log('string:', string);
+  console.log('letter:', letter);
+  if ( letter === string [0] ){
+    return 'true';
+    
+  } //end if
+  else {
+    return 'false';
+  }//end else
+} // end isFirstLetter
 
-
-}
-console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+console.log( 'isFirstLetter - should say true:', isFirstLetter('a', 'apple') );
+console.log( 'isFirstLetter - should say false:', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll(array) {
   let sum = 0
+  console.log('array:', array);
   // TODO: loop to add items
+  for (let i = 0; i < array.length; i++) {
+    sum += array [i];
+    
+  } //end for
   return sum;
 }
+let sumNumbers = [10, 11, 12];
+console.log('sum of array:', sumAll(sumNumbers));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-
-
+function posArray(array) {
+  let positiveNumbers = []
+  console.log('array:', array);
+  for (let i = 0; i < array.length; i++) {
+    if (array [i] > 0){
+      positiveNumbers.push(array [i]);
+    }//end if
+  } //end for
+  return positiveNumbers
+}
+//mixed array
+console.log('Positive numbers from array:', posArray([0, 3, -2, 5, 9])); 
+//all not positive
+console.log('Positive numbers from array:', posArray([0, -3, -2, -5, -9])); 
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
+
+///////Power calculator from edabit/////
+// https://edabit.com/challenge/wAdE9te55cowBLcPs
+// Create a function that takes voltage and current and returns the calculated power.
+// outputs are in KW vs W which is more typically used in the industry.
+//side note: I work as and electrician so I am adding in the phase to determine if it is 3phase or single phase loads.
+function powerCalc(voltage, current, phase) {
+  let kiloWatt = 0
+  console.log('voltage:', voltage, 'current:', current, 'phase:', phase);
+  if (phase === 1){
+    kiloWatt = (voltage * current)/1000;
+
+  } //end if
+  else if (phase === 3){
+    kiloWatt = (voltage * current * 1.732)/1000;
+
+  } //end else if
+  else {
+    kiloWatt = 'invalid input';
+  }
+  return kiloWatt;
+}
+// single phase 230V 10amp load
+console.log('Wattage:', powerCalc(230, 10, 1), 'KW');
+
+// single phase 110V 3amp load
+console.log('Wattage:', powerCalc(110, 3, 1), 'KW');
+
+// three phase 110V 3amp load
+console.log('Wattage:', powerCalc(110, 3, 3), 'KW');
+
+// three phase 480V 20amp load
+console.log('Wattage:', powerCalc(480, 20, 3), 'KW');
+
+// single phase 480V 20amp load
+console.log('Wattage:', powerCalc(480, 20, 1), 'KW');
+
